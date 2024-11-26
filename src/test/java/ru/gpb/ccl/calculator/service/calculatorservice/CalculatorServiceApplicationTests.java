794cbae6e35730e602a906be78988bbabd2dc54c
Oleg.Chumin <oleg.tschumin@gmail.com>
2024-11-26 17:56:47 +0300

    Initial commit

---
+++ src/test/java/ru/gpb/ccl/calculator/service/calculatorservice/CalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gpb.ccl.calculator.service.calculatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
